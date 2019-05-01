42fb56a5c34cb73687d2b01562ee63fff4775201
Renkinjutsu <lucas.cheng12@gmail.com>
2019-05-01 17:52:14 +0000

    char-counting added

---
+++ char-counting.js
@@ -0,0 +1,47 @@
+function countLetters(phrase) {
+  let output = {}
+  let lettersThatHaveAppeared = [];
+  phrase = phrase.toLowerCase();
+  for (let i = 0; i < phrase.length; i++) {
+    if (!lettersThatHaveAppeared.includes(phrase[i])) {
+      output[phrase[i]] = 1
+      lettersThatHaveAppeared.push(phrase[i]);
+    } else {
+      output[phrase[i]] += 1
+    }
+  }
+  return output;
+}
+console.log(countLetters('Finalllfffiinfiinfinallly'));
+
+  /*
+  for (let i = 0; i < phrase.length; i++) {
+    output[phrase[i]] = phrase[i]["count"]
+  }
+// { p : "count"}
+
+for (){
+  output["phrase[i]"] =+ 1
+}
+}
+
+output {
+  a: 0
+}
+var Letter = function (letter) {
+  return {
+  count: 0,
+  countNumber: function(count) {
+    this.count += 1;
+    }
+  }
+}
+var a = new Letter;
+
+console.log(a);
+/* a {
+count:0
+counterNumber()
+}
+
+*/
